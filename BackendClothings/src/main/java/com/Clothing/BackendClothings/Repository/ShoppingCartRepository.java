f71e8d248f97f6c9570253711f0aafc874ab0aa8
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-03-01 00:44:43 +0530

    ShoppingCartRepository added.

--- BackendClothings/src/main/java/com/Clothing/BackendClothings/Repository/ShoppingCartRepository.java
+++ BackendClothings/src/main/java/com/Clothing/BackendClothings/Repository/ShoppingCartRepository.java
@@ -1,4 +1,19 @@
 package com.Clothing.BackendClothings.Repository;
 
-public interface ShoppingCartRepository {
+        import com.Clothing.BackendClothings.Entity.ShoppingCart;
+        import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.data.jpa.repository.Query;
+        import org.springframework.data.repository.query.Param;
+        import org.springframework.stereotype.Repository;
+
+        import java.util.List;
+
+
+@Repository
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer> {
+    //new
+    public List<ShoppingCart> findAllByUserId(long userId);
+
+    ShoppingCart findByUserIdAndProductId(long customer_id, int product_id);
+
 }
