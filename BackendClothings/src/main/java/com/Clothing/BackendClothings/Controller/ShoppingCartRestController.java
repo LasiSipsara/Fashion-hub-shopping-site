e1ae295c6f976ecda2806bc109e7406ade84bef6
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-03-02 09:38:44 +0530

    ShoppingCartRestController changes added

--- BackendClothings/src/main/java/com/Clothing/BackendClothings/Controller/ShoppingCartRestController.java
+++ BackendClothings/src/main/java/com/Clothing/BackendClothings/Controller/ShoppingCartRestController.java
@@ -1,7 +1,12 @@
 package com.Clothing.BackendClothings.Controller;
 
 
+import com.Clothing.BackendClothings.DTO.AddToCartDTO;
+import com.Clothing.BackendClothings.DTO.CustomerDto;
+import com.Clothing.BackendClothings.Entity.Customer;
+import com.Clothing.BackendClothings.Entity.Product;
 import com.Clothing.BackendClothings.Entity.ShoppingCart;
+import com.Clothing.BackendClothings.Service.CustomerImpl;
 import com.Clothing.BackendClothings.Service.CustomerService;
 import com.Clothing.BackendClothings.Service.ProductService;
 import com.Clothing.BackendClothings.Service.ShoppingCartService;
@@ -35,8 +40,48 @@ public class ShoppingCartRestController {
 
 
     @GetMapping("/test")
+    public String test(){
+        return "Hello World";
+    }
+
+
+    @GetMapping(value = "/{customer_id}")
     public ResponseEntity<List<ShoppingCart>> getShoppingCartDetailsByUser (@PathVariable long customer_id){
         List<ShoppingCart> cartList = shoppingCartService.getShoppingCartDetailsByUserId(customer_id);
         return ResponseEntity.ok(cartList);
     }
+
+    @PostMapping("/addToCart/{customer_id}")
+    public String placeOrder(@PathVariable long customer_id, @RequestBody AddToCartDTO addToCartDTO){
+        logger.info("Request Payload" + addToCartDTO.toString());
+        Customer customer = CustomerService.getCustomer(customer_id);
+        Product product = productService.getProductById(addToCartDTO.getProduct_id());
+        ShoppingCart existingCart = null;
+        existingCart = shoppingCartService.FindShoppingCartByCustomerAndProduct(customer_id,product.getProductId());
+        if (existingCart != null){
+            int newQuantity = addToCartDTO.getQuantity()+existingCart.getQuantity();
+            float newAmount = newQuantity * product.getPrice();
+            existingCart.setAmount(newAmount);
+            existingCart.setQuantity(newQuantity);
+            existingCart = shoppingCartService.saveShoppingCart(existingCart);
+            if (existingCart != null){
+                return "Can't update the cart";
+            } else {
+                return "Cart successfully updated";
+            }
+        }
+        else {
+            float amount = product.getPrice() * addToCartDTO.getQuantity();
+
+        }
+
+
+
+
+    }
+
+
+
+
+
 }
